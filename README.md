4c84317a2a1859d81801383b5b0c6d8c88fd6b7c
Guilherme Macena <macenaguilherme1@gmail.com>
2021-10-17 23:28:45 -0300

    Criando repositório para o script do bot de treino do telegram

---
+++ README.md
@@ -0,0 +1,6 @@
+#Telebot_Treino
+
+- Desenvolver um bot usando a api do telegram, que pode auxiliar nos treinos de musculação.
+
+- A ideia é criar um bot que ao ser chamado, possa atuar como um "Instrutor" de academia, 
+passando informações importantes para o usuário que estiver utilizando-o. 
---
+++ telegram_bot_treino/bot_treino.py
@@ -0,0 +1,58 @@
+import telebot
+
+TOKEN = '1959821582:AAHL2Ez5E_bHmaX76i8v5z0ozf39jEX16BY'
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['Bora'])
+def treinar(mensagem):
+    inicio = """
+       VAMOS NESSA!!!
+    
+Vai treinar o que hoje?
+/Bracos      /Pernas
+/Peito      /Costas 
+      /Todos
+"""
+
+    return bot.reply_to(mensagem, inicio)
+
+@bot.message_handler(commands=['Bracos'])
+def treinar_bracos(mensagem):
+    pass
+
+@bot.message_handler(commands=['Pernas'])
+def treinar_pernas(mensagem):
+    pass
+
+@bot.message_handler(commands=['Peito'])
+def treinar_peito(mensagem):
+    pass
+
+@bot.message_handler(commands=['Costas'])
+def treinar_costas(mensagem):
+    pass
+
+@bot.message_handler(commands=['Todos'])
+def treinar_todos(mensagem):
+    pass
+
+
+
+
+def verificar(mensagem):
+    return True
+
+@bot.message_handler(func=verificar) #decorator
+def responder(mensagem):
+    print(mensagem)
+    salve = """
+    Salvee!!    
+    /Bora pro Treino??
+    Digite (/Bora) ou clique.
+    **Qualquer outra coisa não funciona!**"""
+    bot.reply_to(mensagem, salve)
+
+
+bot.polling()
